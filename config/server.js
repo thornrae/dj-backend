0a7756df59659fff1611d47b7c4ce5dbf4eb5043
Taylor Thornton <thornrae@gmail.com>
2022-05-25 22:02:24 -0700

    strapi and cloudinary set up -- must use @3.6.8 for both

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e599b6ea9aa46845891bb1ff1c870e9c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a233e30-9209-4805-9694-62f669a3708f'
+};
