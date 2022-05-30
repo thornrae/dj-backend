db28460d90543051b6c3bfcf61099d8f6647c083
Taylor Thornton <thornrae@gmail.com>
2022-05-30 15:17:13 -0700

    install slugify to create the slug needed for added new event and routing accurately

--- api/events/models/events.js
+++ api/events/models/events.js
@@ -1,8 +1,16 @@
-'use strict';
+const slugify = require("slugify");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+};
